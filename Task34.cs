a0fad88207cd80f9a04ca8c326ba1061fc8ecce9
vs57github <123260473+vs57github@users.noreply.github.com>
2023-02-10 15:05:16 +0300

    Add files via upload

---
+++ Task34.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными
+ трёхзначными числами. Напишите программу, которая покажет 
+ количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+
+using System;
+
+namespace EvenNumberCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[4];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Введите число:");  
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int evenNumberCounter = 0;
+
+            foreach (int number in array) 
+            { 
+                if (number % 2 == 0) 
+                { 
+                    evenNumberCounter++; 
+                } 
+            }
+
+            Console.WriteLine($"Количество четных чисел в массиве: {evenNumberCounter}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
